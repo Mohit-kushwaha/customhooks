a25d92d2ddffb97af5ef569fc8e2354903945516
Mohit-kushwaha <mohit_203015@saitm.org>
2022-08-30 21:10:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
+
+function App()
+{
+  return (
+    <React.Fragment>
+      <BackwardCounter />
+      <ForwardCounter />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
